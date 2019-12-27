9589e7498853ea82959fc35d9784fb48e860158d
Matthew Iannucci <miannucci@navatekltd.com>
2019-12-27 11:19:19 -0500

    Add code and photo template html files

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ app = flask.Flask(__name__)
 def landing():
 	return flask.render_template('landing.html')
 
+@app.route('/code')
+def code():
+	return flask.render_template('code.html')
+
+@app.route('/photography')
+def photography():
+	return flask.render_template('photography.html')
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)

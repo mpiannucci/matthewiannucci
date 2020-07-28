b6bea1aabd80a190b0361a357ee30fc0fe888d87
Matthew Iannucci <mpiannucci@gmail.com>
2020-07-28 20:03:37 +0000

    Small updates

--- main.py
+++ main.py
@@ -21,4 +21,5 @@ def photography():
 
 
 if __name__ == '__main__':
-    app.run(use_reloader=False, debug=True)
+    app.run(host='0.0.0.0', debug=True)
+    #app.run(use_reloader=False, debug=True)

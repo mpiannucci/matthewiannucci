3a9496c510a40a5a2e4010fb388ad09f77ce4cab
Matthew Iannucci <mpiannucci@gmail.com>
2022-02-17 21:38:12 -0500

    Transition to single page layout, start work on content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,21 @@
-<h1 class="text-4xl font-extrabold">Hi, I'm Matt</h1>
 
-<h3 class="text-xl font-bold">
-	I am a software (and ocean) engineer from Rhode Island who enjoys surfing, my dog, and grilling
-	pizzas
-</h3>
+<div class="max-w-5xl mx-auto my-2">
+	<nav>
+		<div class="max-w-5xl mx-auto">
+			<a class="font-bold text-2xl mr-2 hover:text-blue-500" href="/">Matthew Iannucci</a>
+			<a class="text-xl hover:text-blue-500 mx-2" href="/#apps">Apps</a>
+			<a class="text-xl hover:text-blue-500 mx-2" href="/#photos">Photos</a>
+		</div>
+	</nav>
+	<div class="grid md:grid-cols-2 gap-4">
+		<img src="/surfing_portrait_2020.jpg" alt="Portait of myself paddling" />
+		<div>
+			<h1 class="text-4xl font-extrabold">Hi, I'm Matt</h1>
+
+			<h3 class="text-xl font-bold">
+				I am a <code>[software, ocean]</code> engineer from Rhode Island who enjoys surfing, my dog,
+				and grilling pizzas
+			</h3>
+		</div>
+	</div>
+</div>

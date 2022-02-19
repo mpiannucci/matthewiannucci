9989ef0e2d33a28fae761105628f1271f5d1eab1
Matthew Iannucci <mpiannucci@gmail.com>
2022-02-19 11:30:30 -0500

    More responsive

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,13 @@
 
-<div class="max-w-5xl mx-auto my-2">
+<div class="max-w-5xl 2xl:mx-auto mx-4 my-2">
 	<nav>
-		<div class="max-w-2xl 2xl:mx-auto mx-4">
+		<div class="max-w-2xl">
 			<a class="font-bold sm:text-xl md:text-2xl mr-2 hover:text-blue-500" href="/">Matthew Iannucci</a>
 			<a class="sm:text-sm md:text-xl hover:text-blue-500 mx-2" href="/#apps">Apps</a>
 			<a class="sm:text-sm md:text-xl hover:text-blue-500 mx-2" href="/#photos">Photos</a>
 		</div>
 	</nav>
-	<div class="max-w-2xl 2xl:mx-auto mx-4 flex flex-row flex-wrap py-4">
+	<div class="max-w-2xl flex flex-row flex-wrap py-4">
 		<img class="object-cover h-48 w-48 rounded-full sm:self-center" src="/surfing_portrait_2020.jpg" alt="Portait of myself paddling" />
 		<div class="sm:px-4 sm:py-4 max-w-md min-w-sm self-center">
 			<h1 class="text-4xl font-extrabold">Hi, I'm Matt</h1>

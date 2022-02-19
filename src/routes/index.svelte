f56664c63cdf5520b59bf17bc48ba9e961740308
Matthew Iannucci <mpiannucci@gmail.com>
2022-02-19 11:34:36 -0500

    scaffold out portfolio and photos

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,34 @@
-
 <div class="max-w-5xl 2xl:mx-auto mx-4 my-2">
 	<nav>
 		<div class="max-w-2xl">
-			<a class="font-bold sm:text-xl md:text-2xl mr-2 hover:text-blue-500" href="/">Matthew Iannucci</a>
-			<a class="sm:text-sm md:text-xl hover:text-blue-500 mx-2" href="/#apps">Apps</a>
+			<a class="font-bold sm:text-xl md:text-2xl mr-2 hover:text-blue-500" href="/"
+				>Matthew Iannucci</a
+			>
+			<a class="sm:text-sm md:text-xl hover:text-blue-500 mx-2" href="/#portfolio">Portfolio</a>
 			<a class="sm:text-sm md:text-xl hover:text-blue-500 mx-2" href="/#photos">Photos</a>
 		</div>
 	</nav>
-	<div class="max-w-2xl flex flex-row flex-wrap py-4">
-		<img class="object-cover h-48 w-48 rounded-full sm:self-center" src="/surfing_portrait_2020.jpg" alt="Portait of myself paddling" />
-		<div class="sm:px-4 sm:py-4 max-w-md min-w-sm self-center">
-			<h1 class="text-4xl font-extrabold">Hi, I'm Matt</h1>
+	<div class="max-w-2xl">
+		<div id="header" class="flex flex-row flex-wrap py-4">
+			<img
+				class="object-cover h-48 w-48 rounded-full sm:self-center"
+				src="/surfing_portrait_2020.jpg"
+				alt="Portait of myself paddling"
+			/>
+			<div class="sm:px-4 sm:py-4 max-w-md min-w-sm self-center">
+				<h1 class="text-4xl font-extrabold">Hi, I'm Matt</h1>
 
-			<h3 class="text-xl font-bold">
-				I am a <code>[software, ocean]</code> engineer from Rhode Island who enjoys surfing, my dog,
-				and grilling pizzas
-			</h3>
+				<h3 class="text-xl font-bold">
+					I am a <code>[software, ocean]</code> engineer from Rhode Island who enjoys surfing, my dog,
+					and grilling pizzas
+				</h3>
+			</div>
+		</div>
+		<div id="portfolio" class="">
+
+		</div>
+		<div id="photos" class="">
+
 		</div>
 	</div>
 </div>

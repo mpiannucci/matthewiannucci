a5b085c00afa953102b048f0870318d3a7e4a661
Matthew Iannucci <mpiannucci@gmail.com>
2022-02-19 11:27:03 -0500

    More responsive

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,15 @@
 
 <div class="max-w-5xl mx-auto my-2">
 	<nav>
-		<div class="max-w-5xl mx-auto">
-			<a class="font-bold text-2xl mr-2 hover:text-blue-500" href="/">Matthew Iannucci</a>
-			<a class="text-xl hover:text-blue-500 mx-2" href="/#apps">Apps</a>
-			<a class="text-xl hover:text-blue-500 mx-2" href="/#photos">Photos</a>
+		<div class="max-w-2xl 2xl:mx-auto mx-4">
+			<a class="font-bold sm:text-xl md:text-2xl mr-2 hover:text-blue-500" href="/">Matthew Iannucci</a>
+			<a class="sm:text-sm md:text-xl hover:text-blue-500 mx-2" href="/#apps">Apps</a>
+			<a class="sm:text-sm md:text-xl hover:text-blue-500 mx-2" href="/#photos">Photos</a>
 		</div>
 	</nav>
-	<div class="grid md:grid-cols-2 gap-4">
-		<img src="/surfing_portrait_2020.jpg" alt="Portait of myself paddling" />
-		<div>
+	<div class="max-w-2xl 2xl:mx-auto mx-4 flex flex-row flex-wrap py-4">
+		<img class="object-cover h-48 w-48 rounded-full sm:self-center" src="/surfing_portrait_2020.jpg" alt="Portait of myself paddling" />
+		<div class="sm:px-4 sm:py-4 max-w-md min-w-sm self-center">
 			<h1 class="text-4xl font-extrabold">Hi, I'm Matt</h1>
 
 			<h3 class="text-xl font-bold">

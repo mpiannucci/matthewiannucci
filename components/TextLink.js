bb182e3cf7227ae4dd6395e574dd08e99a6955c3
Matthew Iannucci <mpiannucci@gmail.com>
2022-03-12 23:07:24 -0500

    Fix link borders on safari

--- components/TextLink.js
+++ components/TextLink.js
@@ -2,7 +2,7 @@
 const TextLink = ({ title, target }) => (
     <a
         className="text-blue-500 dark:text-blue-300 hover:text-blue-600  dark:hover:text-blue-500
-                     outline outline-2 outline-blue-500 dark:outline-blue-300 hover:outline-blue-600 dark:hover:outline-blue-500
+                     border border-blue-500 dark:border-blue-300 hover:border-blue-600 dark:hover:border-blue-500
                      p-1 rounded-lg font-semibold text-sm"
         href={target}
     >

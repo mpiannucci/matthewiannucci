816b749a8aa0920cda917bc7800b58eaf628dbfb
Matthew Iannucci <mpiannucci@gmail.com>
2022-03-12 23:10:58 -0500

    Tweak link borders a bit

--- components/TextLink.js
+++ components/TextLink.js
@@ -2,7 +2,7 @@
 const TextLink = ({ title, target }) => (
     <a
         className="text-blue-500 dark:text-blue-300 hover:text-blue-600  dark:hover:text-blue-500
-                     border border-blue-500 dark:border-blue-300 hover:border-blue-600 dark:hover:border-blue-500
+                     border-2 border-blue-500 dark:border-blue-300 hover:border-blue-600 dark:hover:border-blue-500
                      p-1 rounded-lg font-semibold text-sm"
         href={target}
     >

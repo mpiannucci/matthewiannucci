b0b208afd28f7de5c618efdcdd54f53e9e0e2607
Matthew Iannucci <mpiannucci@gmail.com>
2023-06-13 00:30:47 -0400

    Update resume and fix hooewaves icon

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import TextLink from '../components/TextLink'
 
 export const metadata = {
     title: 'Matthew Iannucci',
-    description: 'Welcome to my site',
+    description: 'Welcome!',
     favicon: '/favicon.ico',
 };
 
@@ -19,7 +19,7 @@ export default function Index() {
                 <div className="px-4 py-4 max-w-md min-w-sm self-center">
                     <h1 className="text-4xl font-extrabold">Hi, I&lsquo;m Matt</h1>
                     <h3 className="text-xl font-bold">
-                        I am a <code className="font-mono">[software, ocean]</code> engineer from Rhode Island who enjoys surfing, walking my dog,
+                        I am a <code className="font-mono">[software, ocean]</code> engineer from Rhode Island who enjoys surfing, walking my dogs,
                         and grilling pizzas
                     </h3>
                     <div className="flex flex-row space-x-4 py-4">
@@ -38,7 +38,7 @@ export default function Index() {
                             name="HopeWaves"
                             tagline="A local surf forecasting service and app for Rhode Island"
                             //description="This automated surf forecast service pulls in data from NOAA's Wave Watch III wave model and generates a custom surf forecast for the Rhode Island coastline. It also packages local beach cameras, buoy data, tide forecasts, and weather information into a easily digestible and modern interface."
-                            icon="https://hopewaves.app/images/icon.png"
+                            icon="https://hopewaves.app/icon.png"
                             links={[
                                 { title: 'iOS', target: 'https://geo.itunes.apple.com/us/app/hackwinds/id945847570?mt=8' },
                                 { title: 'Android', target: 'https://play.google.com/store/apps/details?id=com.nucc.hackwinds&hl=en&utm_source=global_co&utm_medium=prtnr&utm_content=Mar2515&utm_campaign=PartBadge&pcampaignid=MKT-AC-global-none-all-co-pr-py-PartBadges-Oct1515-1' },
@@ -69,14 +69,6 @@ export default function Index() {
                     <h2 className="text-3xl font-extrabold">Open Source</h2>
                     <div className="flex flex-row flex-wrap">
                         <AppInfo
-                            name="SurfPy"
-                            tagline="A Python library containing math and utilities for generating surf forecasts and monitoring surf conditions."
-                            //The backbone of HopeWaves, this python library is a suite of tools for creating surf forecasting applications. There are tools for downloading buoy data, tide data, wave model data, and more."
-                            links={[
-                                { title: 'View on GitHub', target: 'https://github.com/mpiannucci/surfpy' }
-                            ]}
-                        />
-                        <AppInfo
                             name="React Native Context Menu View"
                             tagline="Use native context menu functionality from React Native."
                             links={[
@@ -96,6 +88,14 @@ export default function Index() {
                             description="WMS router plugin for xpublish"
                             links={[
                                 { title: 'View on GitHub', target: 'https://github.com/xpublish-community/xpublish-wms' }
+                            ]}
+                        />
+                        <AppInfo
+                            name="SurfPy"
+                            tagline="A Python library containing math and utilities for generating surf forecasts and monitoring surf conditions."
+                            //The backbone of HopeWaves, this python library is a suite of tools for creating surf forecasting applications. There are tools for downloading buoy data, tide data, wave model data, and more."
+                            links={[
+                                { title: 'View on GitHub', target: 'https://github.com/mpiannucci/surfpy' }
                             ]}
                         />
                         <AppInfo
